45cdaf3f6c701339fa80b66f443b341cf4536ecb
Ukulele <jeffywolf@gmail.com>
2021-02-12 22:34:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let pluralNoun = "dumplings";
+let name = "Jojo";
+let verb = "obliterate";
+let adjective = "superfluous";
+let color = "periwinkle";
+
+console.log(`JavaScript provides a ${color} collection of tools —\nincluding ${adjective} syntax and ${pluralNoun} — that allows ${name} to ${verb} with strings.`);
